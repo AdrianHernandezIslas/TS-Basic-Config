817458e73cb6c90dafc08eb89d9afecd261b3fb7
Adrian Hernandez Islas <hernandezadri@ext.bancosabadell.mx>
2022-11-16 19:21:19 -0600

    Future(Crud): Producs

--- src/controllers/producto.controller.ts
+++ src/controllers/producto.controller.ts
@@ -2,8 +2,17 @@ import { Request, Response } from "express";
 import { ProductoModel } from "../models/producto.model";
 
 export async function indexViewPoducto(req: Request, res: Response) {
-  const productos = await ProductoModel.findAll({raw:true})
-  return res.render("producto/producto-view",{productos});
+  return res.render("producto/producto-view");
+}
+
+export async function readProducto(req: Request, res: Response) {
+  const {query:where} = req
+  const productos = await ProductoModel.findAll({
+    attributes: ["idProducto", "nombreProducto", "precioCompra", "precioVenta", "existencia", "url_imagen"],
+    raw: true,
+    where
+  });
+  res.status(200).json(productos);
 }
 
 export async function createProducto(req: Request, res: Response) {
@@ -16,3 +25,19 @@ export async function createProducto(req: Request, res: Response) {
     console.log(error);
   }
 }
+
+export async function updateProducto(req: Request, res: Response) {
+  const {idProducto} = req.params;
+  const {body} = req;
+  const entity = await ProductoModel.findByPk(idProducto);
+  await entity?.update(body);
+  res.status(201).json(entity?.toJSON());
+}
+
+export async function deleteProducto(req: Request, res: Response) {
+  const {idProducto} = req.params;
+  const entity = await ProductoModel.findByPk(idProducto);
+  await entity?.destroy();
+  res.status(204).json({ok:""});
+}
+
